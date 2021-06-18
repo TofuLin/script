949f3825380bf8bfcb5772aecb3c14763b8265f2
TofuLin <fjdksl123859@gmail.com>
2021-06-18 12:25:24 +0800

    Update insert_t6.sh

--- insert_t6.sh
+++ insert_t6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..10}
 do
-   psql -c 'insert into towns6code, article, name)select left(md5(i::text), 10), md5(random()::text),md5(random()::text) from generate_series(1, 20000000) s(i);'
+   psql -c 'insert into towns6(code, article, name)select left(md5(i::text), 10), md5(random()::text),md5(random()::text) from generate_series(1, 20000000) s(i);'
 done
