c1d913bf29735c0203eb3c12bbf3b79ff099f1e8
TofuLin <fjdksl123859@gmail.com>
2021-06-18 12:23:48 +0800

    Create insert_t3.sh

---
+++ insert_t3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {1..10}
+do
+   psql -c 'insert into towns3(code, article, name)select left(md5(i::text), 10), md5(random()::text),md5(random()::text) from generate_series(1, 20000000) s(i);'
+done
