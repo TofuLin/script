01900ae7a9d258cdac06d68902cf6e692ace3b41
TofuLin <fjdksl123859@gmail.com>
2021-06-18 12:28:12 +0800

    Delete i#!/bin directory

--- i#!/bin/insert_t9.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..10}
-do
-   psql -c 'insert into towns9(code, article, name)select left(md5(i::text), 10), md5(random()::text),md5(random()::text) from generate_series(1, 20000000) s(i);'
-done
